1649acaaf02dba632386f088755e8c008fe0714a
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-09-14 16:48:22 +0600

    my first commit

---
+++ src/appPages/site/components/layout/LayoutSite.tsx
@@ -0,0 +1,18 @@
+import HomePage from "../pages/HomePage";
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import scss from "./LayoutSite.module.scss";
+
+const LayoutSite = () => {
+  return (
+    <div className={scss.layout}>
+      <Header />
+      <main>
+        <HomePage/>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default LayoutSite;
---
+++ src/appPages/site/components/layout/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import scss from "./footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <h1>footer</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
