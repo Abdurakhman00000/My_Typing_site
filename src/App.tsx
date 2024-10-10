4f4234afc00c6b3bb1241a78d9fa85bfac06fa40
Abdurakhman <abdibaitovabdurahman@gmail.com>
2024-10-10 10:55:56 +0600

    add preload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { useEffect, useState } from "react";
+import PreLoader from "./preLoader/PreLoader";
 
 const App = () => {
+  const [preLoader, setPreLoader] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPreLoader(false);
+    }, 3000);
+  
+    return () => clearTimeout(timer);
+  }, []);
+  
+  if (preLoader) {
+    return <PreLoader/>
+  }
   return <>
     <RouterProvider router={router}/>
   </>;
